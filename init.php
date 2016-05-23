4ebd3cda1e5a18193aad14d6ff01fd7dcd5946af
Alexey Berdnikov <aberdnikov@gmail.com>
2016-05-23 17:08:21 +0500

    fixed

--- init.php
+++ init.php
@@ -14,4 +14,5 @@
     ->js('//cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment-with-locales.min.js')
     ->js('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.15.35/js/bootstrap-datetimepicker.min.js')
     ->css('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.15.35/css/bootstrap-datetimepicker.min.css')
+    ->cssPackage('sf-bootstrap-datetimepicker-init.css')
     ->jsPackage('sf-bootstrap-datetimepicker-init.js');
