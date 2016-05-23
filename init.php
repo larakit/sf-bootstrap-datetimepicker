97d55d35ff06d0ff7f815c418fd2be3b8a3c5283
Alexey Berdnikov <aberdnikov@gmail.com>
2016-05-23 13:25:56 +0500

    fixed

--- init.php
+++ init.php
@@ -10,6 +10,7 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-bootstrap-datetimepicker')
     ->setSourceDir('public')
     ->usePackage('larakit/sf-bootstrap')
+    ->usePackage('larakit/sf-jquery-attrs')
     ->js('//cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment-with-locales.min.js')
     ->js('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.15.35/js/bootstrap-datetimepicker.min.js')
     ->css('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.15.35/css/bootstrap-datetimepicker.min.css')
